d79cff33bc4de10bec4abc1b5c8495c7cdf5b9f2
Hugo Bortolozzi <hugo_bortolozzi@yahoo.fr>
2019-08-13 16:34:40 +0200

    Création d'un page about, donc on change dans le menu le lien vers la page about

--- page-portfolio.php
+++ page-portfolio.php
@@ -3,7 +3,7 @@ get_header();
 ?>
 
 <div class="page-banner" style="background-image: url(<?php the_post_thumbnail_url(); ?>);">
-<?php while (have_posts()): the_post(); ?>
+    <?php while (have_posts()): the_post(); ?>
     <h1>
         <?php the_title(); ?>
     </h1>
@@ -11,7 +11,7 @@ get_header();
 
 </div>
 <div class="container single-post-container">
-<div class="post-content">
+    <div class="post-content">
         <!-- La fonction wp_reset_query() permet de réinitialiser les valeurs $post en fonction du context -->
         <?php wp_reset_query(); ?>
 
@@ -19,7 +19,8 @@ get_header();
         <?php the_content(); ?>
         <?php endwhile; ?>
     </div>
+    <?php get_template_part('templates/partials/grid-portfolio'); ?>
 </div>
 <?php
 get_footer();
-?>  
+?>
