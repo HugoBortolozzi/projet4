98f584890bc4c1827ee50caeead2f2eca6fc1b3d
Hugo Bortolozzi <hugo_bortolozzi@yahoo.fr>
2019-08-13 15:38:51 +0200

    refactoring pour les fonctions de setup du thème

--- functions.php
+++ functions.php
@@ -5,23 +5,13 @@
 
 // nous utilisons le fonction define() de php pour nous facilité l'écriture et pouvoir utiliser un Constante global
 define('INCLUDE_DIR', get_template_directory() . "/includes");
+
 require_once(INCLUDE_DIR . '/enqueue-script.php');
+
 require_once(INCLUDE_DIR . '/menu.php');
 
+require_once(INCLUDE_DIR . '/theme-setup.php');
 
-/**
- * Ajout la fonctionnalité d'un ajout d'image pour les posts pour ce thème ci
- *
- * @return void
- */
-function ajout_image_article()
-{
-  //Ajout de la fonctionnalité d'ajout d'image pour les posts pour ce thème ci
-  add_theme_support('post-thumbnails');
-  $test = 0;
-}
-// Ajout d'un écouteur d'événement pour activer les images mise en avant pour les post (article)
-add_action('init', 'ajout_image_article');
 /**
  * Fonction qui ajoute la possibilité de customiser la partie personnalisation du thème
  * //https://developer.wordpress.org/themes/customize-api/customizer-objects/
