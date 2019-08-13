14a562514e4723b940f992a8434dd4612bebe8c5
Hugo Bortolozzi <hugo_bortolozzi@yahoo.fr>
2019-08-13 15:28:06 +0200

    Ajout d'une variable global afin de nous faciliter l'ecriture

--- includes/menu.php
+++ includes/menu.php
@@ -12,4 +12,4 @@ class MgMenu
     register_nav_menu('main-menu', 'Menu principal dans le header.');
   }
 }
-add_action('after_setup_theme', [MgMenu::class, 'register_main_menu']); 
+add_action('after_setup_theme', [MgMenu::class, 'register_main_menu']);
